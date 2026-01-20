54d1cb48de7db48f974c9f0d5cf3e45eff30cd60
Nitish503 <nitimahto503@gmail.com>
2026-01-20 17:45:44 +0530

    Connect postgreSQL and initializetables

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const express = require("express");
 const { Pool } = require("pg");
 require("dotenv").config();
 const app = express();
+const PORT = process.env.PORT || 10000;
 
 // ==========================
 // MIDDLEWARE
@@ -13,6 +14,7 @@ app.use(express.json());
 const path = require('path');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
 // ================= GET ROUTES =================
 
 // Home page
