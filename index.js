686d325cc13b4cf9b4ce2098a235901fe5798726
Nitish503 <nitimahto503@gmail.com>
2026-01-20 19:27:48 +0530

    Postgres fully configured

--- index.js
+++ index.js
@@ -53,10 +53,14 @@ app.get('/customer', (req, res) => {
 // ==========================
 // DATABASE CONNECTION
 // ==========================
+const { Pool } = require("pg");
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
+  ssl: { rejectUnauthorized: false }
 });
+
+module.exports = pool;
 
 // ==========================
 // DATABASE INITIALIZATION
